92afd587515a17ad728499087a814b0c50b50dad
chemist-god <godtenkorang72@gmail.com>
2025-05-02 23:47:52 +0000

    TodoList contract

---
+++ script/TodoList.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.28;
+
+contract TodoList {
+    // Step 1: Create a Todo Status Enum
+    enum Status { Pending, InProgress, Completed }
+
+    // Step 2: Create a Todo Struct
+    struct Todo {
+        uint id;
+        string content;
+        Status status;
+    }
+
+    // Step 3: Use a Mapping and Array
+    mapping(uint => Todo) public todos;
+    uint[] public todoIds;
+    uint public nextId = 1;
+
+    // Step 5: Define the event
+    event TodoAdded(uint id, string content);
+
+    // Step 4: Implement Basic Functions
+
+    // Function to add a new todo
+    function addTodo(string memory _content) public {
+        todos[nextId] = Todo(nextId, _content, Status.Pending);
+        todoIds.push(nextId);
+        emit TodoAdded(nextId, _content);
+        nextId++;
+    }
+
+    // Function to update the status of a todo
+    function updateStatus(uint _id, Status _status) public {
+        require(bytes(todos[_id].content).length > 0, "Todo does not exist.");
+        todos[_id].status = _status;
+    }
+
+    // Function to get a specific todo
+    function getTodo(uint _id) public view returns (uint, string memory, Status) {
+        require(bytes(todos[_id].content).length > 0, "Todo does not exist.");
+        Todo memory todo = todos[_id];
+        return (todo.id, todo.content, todo.status);
+    }
+}
